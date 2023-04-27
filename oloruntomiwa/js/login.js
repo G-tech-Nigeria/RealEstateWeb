9bed1df15430c6ae8777475e28492637bbc844c2
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-04-27 16:56:34 +0100

    Added features to project

---
+++ oloruntomiwa/js/login.js
@@ -0,0 +1,15 @@
+const passwordInput = document.getElementById("password-input");
+const showPasswordBtn = document.getElementById("show-password-btn");
+
+showPasswordBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (passwordInput.type === 'password') {
+        document.getElementById("eye-off").style.display = "block"
+        document.getElementById("eye-on").style.display = "none"
+        passwordInput.type = 'text';
+      } else {
+        passwordInput.type = 'password';
+        document.getElementById("eye-off").style.display = "none"
+        document.getElementById("eye-on").style.display = "block"
+      }
+});
